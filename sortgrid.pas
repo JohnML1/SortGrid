d60272d0c64a3dfbda90e9f3b59911f903a1705c
John Landmesser <jmlandmesser@gmail.com>
2018-05-19 20:39:25 +0200

    better code, thanks to Werner Pamler

--- sortgrid.pas
+++ sortgrid.pas
@@ -31,6 +31,9 @@ procedure Register;
 
 implementation
 
+uses
+  Math;
+
 procedure Register;
 begin
   {$I sortgrid.lrs}
@@ -39,94 +42,48 @@ end;
 
 { TSortGrid }
 
-function SortStringGridByColumn(Grid: TCustomStringGrid; ACol, ARow, BCol, BRow: Integer): integer;
-var   AFloat, AFloat1 : Double;
-      int, int1 : integer;
-      d, d1 : TDateTime;
-      t, t1 : TTime;
-      s, s1 : string;
+(* Thanks to Werner Pamler :-)  *)
+function GeneralCompareValues(s1, s2: String): Integer;
+var
+  float1, float2: Double;
+  int1, int2: Integer;
+  dt1, dt2: TDateTime;
+  t1, t2: TTime;
 begin
-     try
-
-     (* Spalte für das UpDown-icon *)
-     //FLastColumn := ACol;
-
-
-     Result := 0;
-
-     (* die zwei Zeichenketten in Variable speichern *)
-     s := trim(Grid.Cells[ACol, ARow]);
-     s1 := trim(Grid.Cells[BCol, BRow]);
-
-
-     (* Float *)
-     if TryStrToFloat(s,AFloat) or TryStrToFloat(s1, AFloat1) then
-     begin
-       AFloat := StrToFloatDef(s, 0) - StrToFloatDef(s1, 0);
-       if AFloat < 0 then Result := -1
-       else if AFLoat > 0 then Result := +1
-       else Result := 0;
-       if Grid.SortOrder = soDescending then
-         Result := -Result;
-     end
-     (* Integer *)
-     else if TryStrToInt(s,int) or TryStrToInt(s1, int1) then
-     begin
-       int := StrToIntDef(s, 0) - StrToIntDef(s1, 0);
-       if int < 0 then Result := -1
-       else if int > 0 then Result := +1
-       else Result := 0;
-       if Grid.SortOrder = soDescending then
-         Result := -Result;
-
-     end
-     (* Time *)
-     else if TryStrToTime(s,t) or TryStrToTime(s1, t1) then
-     begin
-       t := StrToTimeDef(s, 0) - StrToTimeDef(s1, 0);
-       if t < 0 then Result := -1
-       else if t > 0 then Result := +1
-       else Result := 0;
-       if Grid.SortOrder = soDescending then
-         Result := -Result;
-     end
-     (* DateTime *)
-     else if TryStrToDateTime(s,d) or TryStrToDateTime(s1, d1) then
-     begin
-       d := StrToDateTimeDef(s, 0) - StrToDateTimeDef(s1, 0);
-       if d < 0 then Result := -1
-       else if d > 0 then Result := +1
-       else Result := 0;
-       if Grid.SortOrder = soDescending then
-         Result := -Result;
-     end
-     else
-     (* String, also alles andere
-      laut Docu Embarcadero: AnsiCompareStr: Compares strings based on the current locale with case sensitivity.
-      *)
-     begin
-       d := AnsiCompareStr(s,s1);
-       if d < 0 then Result := -1
-       else if d > 0 then Result := +1
-       else Result := 0;
-       if Grid.SortOrder = soDescending then
-         Result := -Result;
-     end;
-
-
-
-  finally
+  if (s1 = '') and (s2 = '') then
+    Result := 0
+  else
+  if (s1 = '') then
+    Result := +1
+  else
+  if (s2 = '') then
+    Result := -1
+  else begin
+    if TryStrToFloat(s1, float1) and TryStrToFloat(s2, float2) then
+      Result := CompareValue(float1, float2)
+    else
+    if TryStrToInt(s1, int1) and TryStrToInt(s2, int2) then
+      Result := CompareValue(int1, int2)
+    else
+    if TryStrToDateTime(s1, dt1) and TryStrToDateTime(s2, dt2) then
+      Result := CompareValue(dt1, dt2)
+    else
+    if TryStrToTime(s1, t1) and TryStrToTime(s2, t2) then
+      Result := CompareValue(t1, t2)
+    else
+      Result := CompareStr(s1, s2);
   end;
-
 end;
 
+(* Thanks to Werner Pamler :-)  *)
 function TSortGrid.DoCompareCells(Acol, ARow, Bcol, BRow: Integer): Integer;
+var
+  s1, s2: String;
 begin
-  //Result:=inherited DoCompareCells(Acol, ARow, Bcol, BRow);
-
-  result := 0;
-  Result := SortStringGridByColumn(Self , ACol, ARow, BCol,BRow);
-
+  s1 := TStringGrid(self).Cells[ACol, ARow];
+  s2 := TStringGrid(self).Cells[BCol, BRow];
+  Result := GeneralCompareValues(s1, s2);
+  if TStringGrid(self).SortOrder = soDescending then Result := -Result;
 end;
 
 constructor TSortGrid.Create(AOwner: TComponent);
@@ -140,25 +97,7 @@ begin
   (* optische Rückmeldung, wenn auf Spaltentitel geklickt wurde *)
   Self.Options := Self.Options + [goHeaderPushedLook];
 
-  (* ImageIndex auf 1 setzen *)
-
-  //self.Columns.Clear;
-  //Self.RowCount:=2;
-  //Self.ColCount:=1;
-
   self.FixedCols:=0;
-
-  if  not (csDesigning in ComponentState) then
-  begin
-
-    for x := 0 to 0 do
-    begin
-      self.Columns.Add;
-      self.Columns[x].Title.ImageIndex:=1;
-    end;
-
-  end;
-
 end;
 
 destructor TSortGrid.Destroy;
